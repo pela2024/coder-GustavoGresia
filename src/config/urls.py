0238634d0536561a12157b7189562b890a49f970
_Gustavo Gresia <gustavogresia22@gmail.com>
2024-12-06 02:12:18 -0300

    feat: app core y vistas

--- src/config/urls.py
+++ src/config/urls.py
@@ -16,9 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from core.views import * 
+from core import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("saludar/", saludar)
+    path("saludar/" ,views.saludar),
+    path("saludar-con-etiqueta", views.saludar_con_etiquetas),
+    path("saludar/<str:nombre>/<str:apellido>", views.saludar_con_parametros),
+    path("", views.index), 
+
 ]
